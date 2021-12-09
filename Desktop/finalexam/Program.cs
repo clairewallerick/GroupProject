38b71f26eaf0b6edf53bb7d1490f304fe5630d4a
Claire Wallerick <clairewallerick@Claires-MacBook-Pro-2.local>
2021-12-09 10:55:37 -0600

    first commit

---
+++ Desktop/finalexam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace finalexam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
